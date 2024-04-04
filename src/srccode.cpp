86d6497aed27f24cc77953498c6b71274ac5ca37
Gibson Gelera <76902828+gibsongelera@users.noreply.github.com>
2024-04-04 23:12:09 +0800

    Update srccode.cpp

--- src/srccode.cpp
+++ src/srccode.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int guessNum(int num) {
+    int guess;
+    cout << "Hulaan mo yung number: ";
+    cin >> guess;
+    if(guess > num) cout << "Masyadong Mataas!\n";
+    else if(guess < num) cout << "Masyadong Mababa!\n";
+    return guess;
+}
+
+int main() {
+    srand(time(0)); 
+    int num = rand() % 100 + 1;
+    int guess, tries = 0;
+    do {
+        guess = guessNum(num);
+        tries++;
+    } while(guess != num);
+    cout << "Korek! Nahanap mo yung number " << tries << " ulit.\n";
+    return 0;
+}
 
